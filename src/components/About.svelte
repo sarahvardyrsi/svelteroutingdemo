5fc49ee02233e2936b23be66c24e1411aca2594b
Sarah Vardy <sarah.vardy@ruralsource.com>
2021-01-28 16:47:28 -0600

    first commit

---
+++ src/components/About.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Link } from 'svelte-routing';
+</script>
+
+<div>
+  <h3>This is About Page.</h3>
+  <Link to="/">Home</Link><br />
+  <Link to="chat">Chat</Link><br />
+  <Link to="clock">Clock</Link>
+</div>
+<div class="aboutBody">
+  <p>This is a demo with some super sweet code taken from the online examples and embedded in components.</p>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Router, Route } from 'svelte-routing';
+  import Home from '../components/Home.svelte';
+  import About from '../components/About.svelte';
+  import Chat from '../components/Chat.svelte';
+  import Clock from '../components/Clock.svelte';
+
+  export let url = '';
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="chat" component={Chat} />
+    <Route path="about" component={About} />
+    <Route path="clock" component={Clock} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
